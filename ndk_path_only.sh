d1da2adf0e20a370eab9e7f3a64ec02099168196
jak <jakob@mvm.si>
2022-08-09 14:54:03 +0200

    Fixed prev commit

--- ndk_path_only.sh
+++ ndk_path_only.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NDK_PATH=~/Android/Sdk/ndk/android-ndk-22.1.7171670
+export NDK_PATH=~/Android/Sdk/ndk/22.1.7171670
 export HOST_TAG=linux-x86_64
 
 export TOOLCHAIN=$NDK_PATH/toolchains/llvm/prebuilt/$HOST_TAG
